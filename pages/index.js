a45e5652a6ddb842e21f9ebdef423398aa51912a
Giovani Oliveira <112871572+Giodosvani@users.noreply.github.com>
2024-06-10 21:20:41 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { withRouter } from "next/router";
 
 function Home() {
-    return <h1> Talvez essa seja uma página sobre bolinhos de chuva e café. </h1>
+    return <h1> Essa definitivamente não é uma página sobre bolinhos de chuva e café. </h1>
 }
 
 export default Home;
