338b116a4581a4fb42e672153c10715b7f86bd01
Giovani Oliveira <112871572+Giodosvani@users.noreply.github.com>
2024-06-15 02:00:53 +0000

    frase alterada

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { withRouter } from "next/router";
 
 function Home() {
-    return <h1> A página de uma pessoa muito delicinha. Se você concorda, da uma risadinha.</h1>
+    return <h1> Eu já não tinha juízo, ai chega você com esse sorriso.</h1>
 }
 
 export default Home;
