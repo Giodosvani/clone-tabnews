17e635d419e9561228897065e308bc91f52a122c
Giovani Oliveira <112871572+Giodosvani@users.noreply.github.com>
2024-06-16 15:46:13 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone de um projeto ótimo do curso.dev do Filipe Deschamps. Projeto para aprender do absoluto zero.
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import { withRouter } from "next/router";
 
 function Home() {
-    return <h1>Página feita especificamente pra você, Baixinha kkk.</h1>
+  return <h1>Página feita especificamente pra você, Baixinha kkk.</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
